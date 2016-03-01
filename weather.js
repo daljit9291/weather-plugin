db4af9cc6f03963e0a4da1a75f5ed621e3d61bdd
jaskaranjit <jaskaran.sd@live.ca>
2016-03-01 14:38:25 -0800

    structure completed

---
+++ weather.js
@@ -0,0 +1,19 @@
+
+$(document).ready(function(){
+	var id = $("div[data-type='plugin']");
+ function set(ht){
+ 	$.ajax({
+            type: "GET",
+            url: "http://api.openweathermap.org/data/2.5/weather?q=jalandhar&APPID=9a0bfebddb482e7b0dd96255f3b22f67",
+            datatype: "json",
+            success:function(data){
+            	var date= new Date(data.dt*1000);
+            	$(ht).append("<h1>"+date.getDate()+'/'+(date.getMonth()+1)+'/'+date.getFullYear()+"</h1>");
+            	
+            }
+         });
+        }
+ 
+ 	set(id);
+
+});
